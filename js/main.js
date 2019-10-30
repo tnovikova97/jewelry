e69829b65be0f0238d34c491a85dcd10c5fef1da
Татьяна <t.novikova.2018@stud.nstu.ru>
2019-10-30 21:51:48 +0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#carousel2").owlCarousel({
+        items:1,
+        loop: true
+        // ,
+        // nav: true
+        });
+});
+
+$(document).ready(function(){
+    $("#carousel1").owlCarousel({
+        items:1,
+        loop: true
+    });
+});
