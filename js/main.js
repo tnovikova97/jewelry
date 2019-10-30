9f257d35bc0782d57e8c12296d9bdeb0fc64f232
Татьяна <t.novikova.2018@stud.nstu.ru>
2019-10-30 23:49:27 +0600

    popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Карусель продуктов
+$(document).ready(function(){
+    $("#carousel1").owlCarousel({
+        items:1,
+        loop: true
+    });
+});
+
+// Карусель фотографий
 $(document).ready(function(){
     $("#carousel2").owlCarousel({
         items:1,
@@ -7,9 +16,23 @@ $(document).ready(function(){
         });
 });
 
-$(document).ready(function(){
-    $("#carousel1").owlCarousel({
-        items:1,
-        loop: true
-    });
-});
+// Модальное окно
+
+// открыть по кнопке
+
+// обращаемся к классу элемента кнопки и в метод click помещаем событие
+// элементу body задаем свойство css блюр в 5px - эффект матового стекла
+// при открытие popup окна
+// следующее событие - обращение к элементу overlay и применение метода fadeIn,
+//который позволяет плавно показать окно
+
+// $('.js-button-reg').click(function () {
+//     $('main-content-wrapper').css('filter', 'blur(5px)');
+//     $('js-overlay-reg').fadeIn();
+// });
+//
+// // // Закрыть на крестик
+// $('.js-close-reg').click(function () {
+//     $('.js-overlay-reg').fadeOut();
+//     $('main-content-wrapper').css('filter', 'none');
+// });
