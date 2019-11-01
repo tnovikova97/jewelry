ca339908cc84e9f11ce1cd331dd0a138e0aaa9e8
Татьяна <t.novikova.2018@stud.nstu.ru>
2019-11-01 22:42:14 +0600

    1) Функция js - подключения отдельных js-файлов в main.js
    2) Стили отдельных страниц

---
+++ js/registration-authorization.js
@@ -0,0 +1,48 @@
+// Модальное окно регистрации
+
+// открыть по кнопке
+$('.js-button-reg').click(function () {
+    $('.js-overlay-reg').fadeIn();
+});
+
+// обращаемся к классу элемента кнопки и в метод click помещаем событие
+// элементу body задаем свойство css блюр в 5px - эффект матового стекла
+// при открытие popup окна
+// следующее событие - обращение к элементу overlay и применение метода fadeIn,
+//который позволяет плавно показать окно
+
+// закрыть на крестик
+$('.js-close-reg').click(function () {
+    $('.js-overlay-reg').fadeOut();
+
+});
+
+// закрыть по клику вне окна
+$(document).mouseup(function (e) {
+    var popup = $('.js-popup-reg');
+    if (e.target != popup[0] && popup.has(e.target).length === 0) {
+        $('.js-overlay-reg').fadeOut();
+
+    }
+});
+
+// $('.message a').click(function () {
+//     $('.user-form').animate({height: "toggle", opacity: "toggle"}, "slow");
+// });
+
+// Реализация регистрации и авторизации в одном окне
+$(function () {
+    $('#reg-link').on('click', function (e) {
+        $('.login-form').css('display', 'none');
+        $('.register-form').css('display', 'inline');
+        e.preventDefault()
+    });
+});
+
+$(function () {
+    $('#log-link').on('click', function (e) {
+        $('.register-form').css('display', 'none');
+        $('.login-form').css('display', 'inline');
+        e.preventDefault()
+    });
+});
---
+++ js/tip-of-the-day.js
@@ -0,0 +1,29 @@
+//Совет дня (Tip of the Day)
+
+// Определяем массив
+var tipOfTheDay = ['',
+    '<h1>Совет дня </h1> <br> Украшения - это то, что делает женщищу женщиной <br>(Коко Шанель)',
+    '<h1>Совет дня </h1> <br> Ювелирные украшения - то дополнение, которое дает возможность почувствовать себя особенной <br>(Кэтрин Зета-Джонс)',
+    '<h1>Совет дня </h1> <br> Бриллианты - всегда признак хорошего вкуса <br>(Вудди Аллен)',
+    '<h1>Совет дня </h1> <br> Красота - как драгоценный камень: чем она проще, тем драгоценнее <br>(Фрэнсис Бэкон)',
+];
+
+function RandomTip() {
+    var rnd = Math.floor((Math.random() * 4)) + 1
+    document.getElementById('randomtip').innerHTML = tipOfTheDay[rnd];
+}
+
+// совет дня - открыть по кнопке
+$('.js-btn-totd').click(function () {
+    $('.js-overlay-totd').fadeIn();
+});
+
+
+// совет дня - закрыть по клику вне окна
+$(document).mouseup(function (e) {
+    var popup = $('.js-popup-totd');
+    if (e.target != popup[0] && popup.has(e.target).length === 0) {
+        $('.js-overlay-totd').fadeOut();
+
+    }
+});
