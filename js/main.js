2421610ed6950af58a9efa7368aa2399c070f248
Татьяна <t.novikova.2018@stud.nstu.ru>
2019-11-01 22:23:41 +0600

    общее сохранение captcha

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 // Карусель продуктов
-$(document).ready(function(){
+$(document).ready(function () {
     $("#carousel1").owlCarousel({
-        items:1,
+        items: 1,
         loop: true
     });
 });
 
 // Карусель фотографий
-$(document).ready(function(){
+$(document).ready(function () {
     $("#carousel2").owlCarousel({
-        items:1,
+        items: 1,
         loop: true
         // ,
         // nav: true
-        });
+    });
 });
 
+
+// Модальное окно регистрации
+
+// открыть по кнопке
+$('.js-button-reg').click(function () {
+    $('.js-overlay-reg').fadeIn();
+});
 
 // обращаемся к классу элемента кнопки и в метод click помещаем событие
 // элементу body задаем свойство css блюр в 5px - эффект матового стекла
@@ -23,17 +30,8 @@ $(document).ready(function(){
 // следующее событие - обращение к элементу overlay и применение метода fadeIn,
 //который позволяет плавно показать окно
 
-
-
-// Модальное окно регистрации
-
-// открыть по кнопке
-$('.js-button-reg').click(function() {
-    $('.js-overlay-reg').fadeIn();
-});
-
 // закрыть на крестик
-$('.js-close-reg').click(function() {
+$('.js-close-reg').click(function () {
     $('.js-overlay-reg').fadeOut();
 
 });
@@ -41,7 +39,7 @@ $('.js-close-reg').click(function() {
 // закрыть по клику вне окна
 $(document).mouseup(function (e) {
     var popup = $('.js-popup-reg');
-    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    if (e.target != popup[0] && popup.has(e.target).length === 0) {
         $('.js-overlay-reg').fadeOut();
 
     }
@@ -71,7 +69,7 @@ $(function () {
 //Совет дня (Tip of the Day)
 
 // Определяем массив
-var tipOfTheDay = [ '',
+var tipOfTheDay = ['',
     '<h1>Совет дня </h1> <br> Украшения - это то, что делает женщищу женщиной <br>(Коко Шанель)',
     '<h1>Совет дня </h1> <br> Ювелирные украшения - то дополнение, которое дает возможность почувствовать себя особенной <br>(Кэтрин Зета-Джонс)',
     '<h1>Совет дня </h1> <br> Бриллианты - всегда признак хорошего вкуса <br>(Вудди Аллен)',
@@ -84,7 +82,7 @@ function RandomTip() {
 }
 
 // совет дня - открыть по кнопке
-$('.js-btn-totd').click(function() {
+$('.js-btn-totd').click(function () {
     $('.js-overlay-totd').fadeIn();
 });
 
@@ -92,16 +90,43 @@ $('.js-btn-totd').click(function() {
 // совет дня - закрыть по клику вне окна
 $(document).mouseup(function (e) {
     var popup = $('.js-popup-totd');
-    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    if (e.target != popup[0] && popup.has(e.target).length === 0) {
         $('.js-overlay-totd').fadeOut();
 
     }
 });
 
 
+// создание captcha
+
+var captchaA = randomNumber(1,10); // генерируем число
+var captchaB = randomNumber(1,10); // генерируем число
+var captchaAB = captchaA + captchaB;  // вычисляем сумму
+document.getElementById('captcha-span').innerHTML = captchaA + ' + ' + captchaB + ' = ';  // показываем пользователю выражение
+
+// function ValidCaptcha() {
+//     var code1 = sumAB;
+//     var code2 = document.getElementById('txtInput').value;
+//     if (code1 == code2) {
+//         $('.captache-error').hide();
+//         $('.formcontent').slideDown();
+//     } else {
+//         $('.captache-error').show();
+//         $('.formcontent').hide();
+//     }
+// }
+
+// функция для генерации случайных чисел в диапазоне от m до n
+function randomNumber(m,n){
+    m = parseInt(m);
+    n = parseInt(n);
+    return Math.floor( Math.random() * (n - m + 1) ) + m;
+};
+
+
 //Валидация формы регистрации
 
-// getting all input text objects
+// получение всех входных текстовых объектов
 var username = document.forms["reg-form"]["username"];
 var surname = document.forms["reg-form"]["surname"];
 var email = document.forms["reg-form"]["email"];
@@ -109,7 +134,7 @@ var login = document.forms["reg-form"]["login"];
 var password = document.forms["reg-form"]["password"];
 var password2 = document.forms["reg-form"]["password2"];
 
-//getting all errors display objects
+//получение всех объектов отображения ошибок
 var username_error = document.getElementById("username_error");
 var surname_error = document.getElementById("surname_error");
 var email_error = document.getElementById("email_error");
@@ -117,7 +142,7 @@ var login_error = document.getElementById("login_error");
 var password_error = document.getElementById("password_error");
 var password2_error = document.getElementById("password2_error");
 
-//setting all event listeners
+//установка обработчиков
 username.addEventListener("blur", usernameVerify, true);
 surname.addEventListener("blur", surnameVerify, true);
 email.addEventListener("blur", emailVerify, true);
@@ -125,7 +150,7 @@ login.addEventListener("blur", loginVerify, true);
 password.addEventListener("blur", passwordVerify, true);
 password2.addEventListener("blur", password2Verify, true);
 
-//validation function
+//Функция валидации регистрации
 function validate_reg() {
     //username validation
     if (username.value == "") {
@@ -171,12 +196,24 @@ function validate_reg() {
         password.focus();
         return false;
     }
-    return true;
+
+//    проверка капчи
+    var code1 = captchaAB;
+    var code2 = document.getElementById('txtInput').value;
+    if (code1 == code2) {
+        $('.captcha-error').hide();
+        $('.captcha-right').show();
+    } else {
+        $('.captcha-error').show();
+        $('.captcha-right').hide();
+        event.preventDefault()
+    }
+return true
 }
 
-//event handler function
+//функции-обработчики
 function usernameVerify() {
-    if (username.value !="") {
+    if (username.value != "") {
         username.style.border = "1px solid #5E6E66";
         username_error.innerHTML = "";
         return true;
@@ -184,7 +221,7 @@ function usernameVerify() {
 }
 
 function surnameVerify() {
-    if (surname.value !="") {
+    if (surname.value != "") {
         surname.style.border = "1px solid #5E6E66";
         surname_error.innerHTML = "";
         return true;
@@ -192,7 +229,7 @@ function surnameVerify() {
 }
 
 function emailVerify() {
-    if (email.value !="") {
+    if (email.value != "") {
         email.style.border = "1px solid #5E6E66";
         email_error.innerHTML = "";
         return true;
@@ -200,7 +237,7 @@ function emailVerify() {
 }
 
 function loginVerify() {
-    if (login.value !="") {
+    if (login.value != "") {
         login.style.border = "1px solid #5E6E66";
         login_error.innerHTML = "";
         return true;
@@ -208,7 +245,7 @@ function loginVerify() {
 }
 
 function passwordVerify() {
-    if (password.value !="") {
+    if (password.value != "") {
         password.style.border = "1px solid #5E6E66";
         password_error.innerHTML = "";
         return true;
@@ -216,67 +253,57 @@ function passwordVerify() {
 }
 
 function password2Verify() {
-    if (password2.value !="") {
+    if (password2.value != "") {
         password2.style.border = "1px solid #5E6E66";
         password2_error.innerHTML = "";
         return true;
     }
 }
 
-// Валидация формы авторизации
-var login_auth = document.forms["auth-form"]["login_auth"];
-var password_auth = document.forms["auth-form"]["password_auth"];
-
-var login_auth_error = document.getElementById("login_auth_error");
-var password_auth_error = document.getElementById("password_auth_error");
-
-login_auth.addEventListener("blur", loginVerify, true);
-password_auth.addEventListener("blur", passwordVerify, true);
-
-
-function validate_auth() {
-    //login validation
-    if (login_auth.value == "") {
-        login_auth.style.border = "3px solid #FF1F1F";
-        login_auth_error.textContent = "Введите логин";
-        login_auth.focus();
-        return false;
-    }
-    //password validation
-    if (password_auth.value == "") {
-        password_auth.style.border = "3px solid #FF1F1F";
-        password_auth_error.textContent = "Введите пароль";
-        password_auth.focus();
-        return false;
-    }
-    return true;
-}
-
-function loginVerify() {
-    if (login_auth.value !="") {
-        login_auth.style.border = "1px solid #5E6E66";
-        login_auth_error.innerHTML = "";
-        return true;
-    }
-}
-
-function passwordVerify() {
-    if (password_auth.value !="") {
-        password_auth.style.border = "1px solid #5E6E66";
-        password_auth_error.innerHTML = "";
-        return true;
-    }
-}
-
-
-
-
-
-
-
-
-
-
+// // Валидация формы авторизации
+// var login_auth = document.forms["auth-form"]["login_auth"];
+// var password_auth = document.forms["auth-form"]["password_auth"];
+//
+// var login_auth_error = document.getElementById("login_auth_error");
+// var password_auth_error = document.getElementById("password_auth_error");
+//
+// login_auth.addEventListener("blur", loginVerify, true);
+// password_auth.addEventListener("blur", passwordVerify, true);
+//
+//
+// function validate_auth() {
+//     //login validation
+//     if (login_auth.value == "") {
+//         login_auth.style.border = "3px solid #FF1F1F";
+//         login_auth_error.textContent = "Введите логин";
+//         login_auth.focus();
+//         return false;
+//     }
+//     //password validation
+//     if (password_auth.value == "") {
+//         password_auth.style.border = "3px solid #FF1F1F";
+//         password_auth_error.textContent = "Введите пароль";
+//         password_auth.focus();
+//         return false;
+//     }
+//     return true;
+// }
+//
+// function loginVerify() {
+//     if (login_auth.value != "") {
+//         login_auth.style.border = "1px solid #5E6E66";
+//         login_auth_error.innerHTML = "";
+//         return true;
+//     }
+// }
+//
+// function passwordVerify() {
+//     if (password_auth.value != "") {
+//         password_auth.style.border = "1px solid #5E6E66";
+//         password_auth_error.innerHTML = "";
+//         return true;
+//     }
+// }
 
 
 
